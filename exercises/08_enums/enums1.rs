ef7ee77c2bd8abfbf37364a42f039692b5a8e3e4
Kateryna Laptieva <laptievakate@gmail.com>
2025-11-09 13:47:19 +0100

    07, 08

--- exercises/08_enums/enums1.rs
+++ exercises/08_enums/enums1.rs
@@ -1,12 +1,18 @@
 #[derive(Debug)]
 enum Message {
     // TODO: Define a few types of messages as used below.
+    Resize,
+    Quit,
+    Move,
+    Echo,
+    
+    ChangeColor(u8, u8, u8), 
 }
 
 fn main() {
     println!("{:?}", Message::Resize);
     println!("{:?}", Message::Move);
     println!("{:?}", Message::Echo);
-    println!("{:?}", Message::ChangeColor);
+    println!("{:?}", Message::ChangeColor(255, 100, 50)); 
     println!("{:?}", Message::Quit);
 }
